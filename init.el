6eac05b0610b6cdfc85f4397f4ed3cc1bcec59ab
Matthew Soulanille <matthew@soulanille.net>
2019-02-07 15:46:16 -0500

    Fix company for eclim-mode (and also c / c++ mode by chance)

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck-rust rust-mode rjsx-mode flow-minor-mode web-mode multiple-cursors flycheck-nim nim-mode flymake-jslint flymake-jshint smart-tabs-mode cmake-mode sage-shell-mode flymd company-irony company sml-mode tide irony bison-mode typescript forth-mode julia-mode markdown-mode racket-mode ## opencl-mode auctex haskell-mode lua-mode js2-mode))))
+	(company-emacs-eclim eclim flycheck-rust rust-mode rjsx-mode flow-minor-mode web-mode multiple-cursors flycheck-nim nim-mode flymake-jslint flymake-jshint smart-tabs-mode cmake-mode sage-shell-mode flymd company-irony company sml-mode tide irony bison-mode typescript forth-mode julia-mode markdown-mode racket-mode ## opencl-mode auctex haskell-mode lua-mode js2-mode))))
 (package-initialize)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -76,12 +76,16 @@
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-mode-map [remap indent-for-tab-command] 'company-indent-or-complete-common)
+  (define-key company-mode-map [remap c-indent-line-or-region] 'company-indent-or-complete-common)
   ()
   )
 
 
+(setq-default tab-width 4) ; or any other preferred value
+(setq cua-auto-tabify-rectangles nil)
 
-(smart-tabs-insinuate 'c++)
+(smart-tabs-insinuate 'c++ 'javascript 'java 'c)
+(smart-tabs-advice js2-indent-line js2-basic-offset)
 
 (global-set-key (kbd "C-c C-r") 'mc/mark-all-like-this) ; Ctrl+c r
 (global-set-key (kbd "C-c C-l") 'mc/edit-lines)
@@ -124,8 +128,36 @@
   :bind ("C-c C-c" . compile)
   :bind ("C-c C-f" . tide-fix)
   )
+;; (with-eval-after-load "tide"
+;;   (define-key tide-mode-map "C-c C-c" 'compile)
+;;   (define-key tide-mode-map "C-c C-f" 'tide-fix)
+;;   )
 
 
 
 
 ; End Typescript
+(put 'downcase-region 'disabled nil)
+
+
+; Flymd browser
+(defun my-flymd-browser-function (url)
+  (let ((process-environment (browse-url-process-environment)))
+    (apply 'start-process
+           (concat "firefox " url)
+           nil
+           "/usr/bin/open"
+           (list "-a" "firefox" url))))
+(setq flymd-browser-open-function 'my-flymd-browser-function)
+
+; Company Eclim for java
+(custom-set-variables
+ '(eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse"))
+ '(eclim-executable "/Users/matthew/.p2/pool/plugins/org.eclim_2.8.0/bin/eclim")
+ )
+(company-emacs-eclim-setup)
+(add-hook 'java-mode-hook 'eclim-mode)
+
+
+
+;(require 'eclimd)
