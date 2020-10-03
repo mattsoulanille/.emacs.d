c70a150ced24242c171df0913c11dede08558f47
Matthew Soulanille <matthew@soulanille.net>
2020-10-02 20:46:01 -0700

    Rust code completion

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (package-initialize)
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages/")
+;			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 
 
@@ -16,9 +16,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-	(company-emacs-eclim eclim flycheck-rust rust-mode rjsx-mode flow-minor-mode web-mode multiple-cursors flycheck-nim nim-mode flymake-jslint flymake-jshint smart-tabs-mode cmake-mode sage-shell-mode flymd company-irony company sml-mode tide irony bison-mode typescript forth-mode julia-mode markdown-mode racket-mode ## opencl-mode auctex haskell-mode lua-mode js2-mode))))
+	(magit crontab-mode eglot yaml-mode toml-mode company-lsp lsp-ui lsp-mode rustic rust-auto-use flycheck-demjsonlint bazel-mode protobuf-mode company-jedi company-emacs-eclim eclim flycheck-rust rust-mode rjsx-mode flow-minor-mode web-mode multiple-cursors flycheck-nim nim-mode flymake-jslint flymake-jshint smart-tabs-mode cmake-mode sage-shell-mode flymd company-irony company sml-mode tide irony bison-mode typescript forth-mode julia-mode markdown-mode racket-mode ## opencl-mode auctex haskell-mode lua-mode js2-mode))))
 (package-initialize)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -27,8 +28,7 @@
  ;; If there is more than one, they won't work right.
  )
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js-mode))
 
 
 
@@ -54,6 +54,7 @@
 
 ;; == company-mode ==
 (use-package company
+  :hook (prog-mode . company-mode)
   :ensure t
   :defer t
   :init (add-hook 'after-init-hook 'global-company-mode)
@@ -79,6 +80,67 @@
   (define-key company-mode-map [remap c-indent-line-or-region] 'company-indent-or-complete-common)
   ()
   )
+
+; Flycheck
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
+
+; lsp
+(use-package lsp-mode
+  :commands lsp
+  :config (require 'lsp-clients))
+
+(use-package lsp-ui)
+;; Allow company to get stuff from lsp
+(use-package company-lsp)
+
+;; lsp keybinds
+(with-eval-after-load "lsp-mode"
+  ;;(bind-key "C-c C-c" 'compile tide-mode-map)
+  (bind-key "C-c C-f" 'lsp-execute-code-action lsp-mode-map)
+  )
+
+
+;; Rust stuff
+(use-package toml-mode)
+
+;; (use-package rustic-mode
+;;   :hook (rustic-mode . lsp)
+;;   :config
+;;   (bind-key "C-c C-r" 'rustic-run rustic-mode-map)
+;;   (bind-key "C-c C-f" 'lsp-execute-code-action rustic-mode-map)
+;;   )
+
+(use-package rustic
+  :commands (cargo-minor-mode)
+  :mode ("\\.rs" . rustic-mode)
+  :config
+  (bind-keys :map rustic-mode-map
+             ("C-c TAB" . rustic-format-buffer)
+             ("C-c C-f" . lsp-execute-code-action)
+			 ("C-c C-c" . rustic-cargo-build)
+			 ("C-c f" . rustic-format-buffer))
+  :init
+  (setq company-tooltip-align-annotations t
+        rustic-format-on-save nil)
+  (add-hook 'rustic-mode-hook #'cargo-minor-mode)
+  (add-hook 'rustic-mode-hook #'flycheck-mode)
+  (add-hook 'rustic-mode-hook #'lsp)
+;  (add-hook 'rustic-mode-hook #'lsp-ui)
+  )
+
+;; (with-eval-after-load "rustic-mode"
+;;   ;;(bind-key "C-c C-c" 'compile tide-mode-map)
+;;   )
+
+;; Add keybindings for interacting with Cargo
+(use-package cargo
+  :hook (rustic-mode . cargo-minor-mode))
+
+;;(use-package flycheck-rust
+;;  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+
 
 
 (setq-default tab-width 4) ; or any other preferred value
@@ -145,29 +207,56 @@
 
 
 ;; Flymd browser
+;; (defun my-flymd-browser-function (url)
+;;   (let ((process-environment (browse-url-process-environment)))
+;;     (apply 'start-process
+;;            (concat "firefox " url)
+;;            nil
+;;            "/usr/bin/open"
+;;            (list "-a" "firefox" url))))
+;; (setq flymd-browser-open-function 'my-flymd-browser-function)
+
 (defun my-flymd-browser-function (url)
-  (let ((process-environment (browse-url-process-environment)))
-    (apply 'start-process
-           (concat "firefox " url)
-           nil
-           "/usr/bin/open"
-           (list "-a" "firefox" url))))
+  (let ((browse-url-browser-function 'browse-url-firefox))
+    (browse-url url)))
 (setq flymd-browser-open-function 'my-flymd-browser-function)
 
+;; Company Jedi for python
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+(setq python-shell-interpreter "python3")
+
+(defun cm:jedi-setup-server ()
+  "Setup Jedi server, update and reload."
+  (interactive)
+  (if (not (equal python-environment-default-root-name (cm:jedi-environment-root-name)))
+      (progn
+        (custom-set-variables
+         '(python-environment-default-root-name (cm:jedi-environment-root-name))
+         '(jedi:server-command
+           (concat (venv-name-to-dir (cm:jedi-environment-root-name)) "bin/jediepcserver")))
+        (jedi:install-server-block))))
+
+
+
 ;; Company Eclim for java
-(setq eclimd-autostart t)
-(custom-set-variables
- '(eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse"))
- '(eclim-executable "/Users/matthew/.p2/pool/plugins/org.eclim_2.8.0/bin/eclim")
- )
-(company-emacs-eclim-setup)
-(add-hook 'java-mode-hook 'eclim-mode)
-(with-eval-after-load "eclim"
-  ;;  (define-key eclim-mode-map "C-c C-c" 'eclim-project-build)
-  (bind-key "C-c C-c" 'eclim-project-build eclim-mode-map)
-  (bind-key "C-c C-f" 'eclim-problems-correct eclim-mode-map)
-  )
 
+;; (setq eclimd-autostart t)
+;; (custom-set-variables
+;;  '(eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse"))
+;;  '(eclim-executable "/Users/matthew/.p2/pool/plugins/org.eclim_2.8.0/bin/eclim")
+;;  )
+;; (company-emacs-eclim-setup)
+;; (add-hook 'java-mode-hook 'eclim-mode)
+;; (with-eval-after-load "eclim"
+;;   ;;  (define-key eclim-mode-map "C-c C-c" 'eclim-project-build)
+;;   (bind-key "C-c C-c" 'eclim-project-build eclim-mode-map)
+;;   (bind-key "C-c C-f" 'eclim-problems-correct eclim-mode-map)
+;;   )
 
+;; Bazel BUILD file formatting
+(add-hook 'bazel-mode-hook (lambda () (add-hook 'before-save-hook #'bazel-format nil t)))
 
 ;; (require 'eclimd)
