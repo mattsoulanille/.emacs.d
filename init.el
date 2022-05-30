7eedd405ba06514bcc0c76cc3b5d56982185b0da
Matthew Soulanille <matthew@soulanille.net>
2022-05-30 16:26:41 -0700

    lsp

--- init.el
+++ init.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(setq column-number-mode t)
+
 (use-package auto-package-update
   :ensure
   :config
@@ -29,6 +31,48 @@
 
 (use-package flycheck :ensure)
 
+(use-package which-key
+  :ensure
+  :config (which-key-mode))
+(use-package project :ensure)
+(use-package lsp-mode
+  :ensure
+  :init (setq lsp-keymap-prefix "C-c l")
+  :commands lsp
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (python-mode . lsp)
+         (c++-mode . lsp)
+         (js-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :bind (:map lsp-mode-map
+	      ("C-c C-f" . lsp-execute-code-action))
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; This controls the overlays that display type and other hints inline. Enable
+  ;; / disable as you prefer. Well require a `lsp-workspace-restart' to have an
+  ;; effect on open projects.
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
 ;; TypeScript
 (use-package typescript-mode :ensure)
 (use-package tide
@@ -45,7 +89,8 @@
 	    (setq flycheck-check-syntax-automatically '(save mode-enabled))
 	    (eldoc-mode +1)
 	    (tide-hl-identifier-mode +1)
-	    (company-mode +1)))
+	    (company-mode +1)
+	    (display-fill-column-indicator-mode)))
 
 
 (use-package magit :ensure)
@@ -57,11 +102,13 @@
            (setq js-indent-level 2))))
 
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fill-column 80)
  '(package-selected-packages '(use-package cmake-mode auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
